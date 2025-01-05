154998eac518a8966007b7d87a48e7a9b8ecb8fd
Farkhan1220 <farkhanamarrudin8@gmail.com>
2025-01-05 23:31:12 +0700

    Update streamlit (2).py

--- streamlit (2).py
+++ streamlit (2).py
@@ -1,12 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.linear_model import LinearRegression
 
 # Streamlit App Title
 st.title('Linear Regression Weather Prediction')
